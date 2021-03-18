02e1e382626e2913af3874c3877f3b65fceb4636
francesta <huntleyfrancesta@gmail.com>
2021-03-18 11:48:32 -0400

    git@github.com:huntleyfrancesta/phase-0-intro-to-js-2-objects-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+    city: 'New York',
+    state: 'NY',
+    zip: '10004'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const nextEmployee = {...obj };
+
+    nextEmployee[key] = value;
+    return nextEmployee;
+}
+/*const employee1 = updateEmployeeWithKeyAndValue(employee,'name', 'Sam');
+console.log(employee1);
+console.log(nextEmployee);*/
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+/*const employee2 = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'jessica howard');
+console.log(employee2);
+console.log(employee);*/
+
+function deleteFromEmployeeByKey(obj, key) {
+    const employee3 = {...obj }
+    delete employee3[key];
+    return employee3;
+}
+/*const employee3 = deleteFromEmployeeByKey(employee1, 'zip');
+console.log(employee3);
+console.log(employee1);*/
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+/*destructivelyDeleteFromEmployeeByKey(employee1, 'zip');
+console.log(employee1);*/
